8cef3ddabba592f69b0ead26217e3574ed973227
Jen J Wong <jenjwong@gmail.com>
2017-06-19 19:46:47 -0700

    Refactors for style

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,3 @@
-export const REQUEST_SUBMIT = 'REQUEST_SUBMIT';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
 export const GET_EXPENSES = 'GET_EXPENSES';
